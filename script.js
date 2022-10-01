b6faf0b7d77d29e805f0b11679eb359b6ab42b22
Lucas <lmsousa20@gmail.com>
2022-10-01 17:08:05 -0300

    Metodo post

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
-let promessa = fetch("https://633867b7937ea77bfdbf9c85.mockapi.io/pessoa");
-console.log(promessa);
+
+
+const executar = (resposta) => {
+    
+    return resposta.json();
+}
+const atualizar = () => {
+    lista.innerHTML='Carregando...'
+    let promessa = fetch("https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa");
+    let promessa2 = promessa.then(executar);
+    promessa2.then(objetoRetornadoJson => {
+        lista.innerHTML='';
+        objetoRetornadoJson.forEach((pessoa) => {
+            lista.innerHTML += "<li>Nome: " + pessoa.nome + " - Idade:" + pessoa.idade + "</li>"
+        })
+    
+    })
+}
+const adicionar =()=>{
+    let nomeUsuario=usuario.value;
+    let requestPost={
+        method:"POST",
+        headers:{"content-type":"application/json"},
+        body:JSON.stringify({
+            nome:nomeUsuario,
+            idade:20
+        })
+    } 
+    promessaPost= fetch("https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa",requestPost).then(atualizar())
+    console.log(promessaPost)
+    
+}
+
+
+
